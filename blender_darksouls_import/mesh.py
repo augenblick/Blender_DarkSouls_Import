992844daf327fb696a2d57ab116577916c59cb98
Aaron Askew <aaronaskew@gmail.com>
2020-04-27 01:44:05 -0500

    Added class skeletons for Model, Mesh, Face, Vertex

---
+++ blender_darksouls_import/mesh.py
@@ -0,0 +1,36 @@
+import face
+import vertex
+
+
+class Mesh:
+    """A class that represents a mesh in Blender."""
+
+    vertices = []
+    faces = []
+
+    def __init__(self, vertices=None, faces=None):
+        """
+
+        :param vertices: List of Vertex objects
+        :param faces: List of Face objects
+        """
+
+        if vertices is not None:
+
+            # validate vertices
+            if len(vertices) < 3:
+                raise ValueError("At least three vertices expected")
+            for v in vertices:
+                if not isinstance(v, Vertex):
+                    raise TypeError("Object of type Vertex expected, however type {} was passed".format(type(m)))
+
+            self.vertices = vertices
+
+        if faces is not None:
+            # validate faces
+            for f in faces:
+                if not isinstance(f, Face):
+                    raise TypeError("Object of type Face expected, however type {} was passed".format(type(m)))
+
+            self.faces = faces
+
---
+++ blender_darksouls_import/model.py
@@ -0,0 +1,20 @@
+import mesh
+import vertex
+import face
+
+
+class Model:
+    """A class that represents a Model in Blender."""
+
+    meshes = []
+
+    def __init__(self, meshes=None):
+        """
+
+        :param meshes: List of Mesh objects.
+        """
+
+        # validate meshes
+        for m in meshes:
+            if not isinstance(m, Mesh):
+                raise TypeError("Objects of type Mesh expected, however type {} was passed".format(type(m)))
