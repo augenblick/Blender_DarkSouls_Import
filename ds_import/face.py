e4d57c3338f31bfd1a46c69ed6bf6ff0c5a3b744
Aaron Askew <aaronaskew@gmail.com>
2020-05-03 14:38:48 -0500

    Changed face.py
    Updated face.py vertices memeber to represent index values in a list of vertices.

--- ds_import/face.py
+++ ds_import/face.py
@@ -4,22 +4,31 @@ from vertex import Vertex
 class Face:
     """A class that represents a triangular face in Blender."""
 
-    vertices = []
-
     def __init__(self, vertices=None):
         """
 
-        :param vertices: List of three Vertex objects.
+        :param vertices: List of unsigned ints, each representing the index of a list of Vertex objects.
         """
         if vertices is not None:
             # validate vertices
             if len(vertices) != 3:
-                raise ValueError('Three vertices expected.')
-            for vertex in vertices:
-                if not isinstance(vertex, Vertex):
-                    raise TypeError('Objects of type Vertex expected, however type {} was passed'.format(type(vertex)))
+                raise ValueError('Three unsigned integers expected.')
+            for vertex_index in vertices:
+                if not isinstance(vertex_index, int):
+                    raise TypeError('Objects of type int expected, however type {} was passed'.format(type(vertex_index)))
 
-            self.vertices = vertices
+            self._vertices = vertices
+        else:
+            self._vertices = None
+
+    @property
+    def vertices(self):
+        """A list of unsigned integers representing the indexes of an array of Vertex objects"""
+        return self._vertices
+
+    @vertices.setter
+    def vertices(self, vertices):
+        self._vertices = vertices
 
     def blender_ordered_vertices(self):
         """Returns the vertices in an order that Blender expects."""
